262d69eb104946b208b2d5e0ffa8605a3f5a6b64
krishnan <krishnan57474@gmail.com>
2018-10-31 16:58:05 +0530

    Added separate css

---
+++ gaur/application/views/app/default/admin/common/css.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Gaur
+ *
+ * An open source web application
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2017 - 2018, Krishnan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    Gaur
+ * @author     Krishnan <krishnan57474@gmail.com>
+ * @copyright  Copyright (c) 2017 - 2018, Krishnan
+ * @license    https://opensource.org/licenses/MIT   MIT License
+ * @link       https://github.com/krishnan57474
+ * @since      Version 3.0.0
+ */
+
+defined('BASEPATH') OR exit;
+
+?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/twbs/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/smartmenus.css">
+
+    <script type="text/x-async-js" data-src="https://cdn.jsdelivr.net/gh/jquery/jquery@3.3.1/dist/jquery.min.js" data-integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" class="j-ajs"></script>
