dd1841f3bec77258b8fd61f7eeda50264a573943
krishnan <krishnan57474@gmail.com>
2018-10-31 17:17:45 +0530

    Added attachments

--- gaur/application/views/email/default/contact.php
+++ gaur/application/views/email/default/contact.php
@@ -40,7 +40,7 @@ $this->load->view('email/default/common/head');
 
 ?>
 <tr>
-<td style="background-color:#f6f6f7;padding:30px 20px 10px">
+<td style="background-color:#f6f6f7;padding:20px 20px 10px">
 <p style="color:#465059;font-size:24px;font-weight:bold;text-align:center;margin:0">
 Enquiry email
 </p>
@@ -53,15 +53,35 @@ Enquiry email
 </p>
 </td>
 </tr>
-<?php if ($finputs): ?>
 <tr>
-<td style="background-color:#fff;padding:20px">
+<td style="background-color:#fff;padding:20px 20px 0">
 <table cellspacing="0" cellpadding="0" style="margin:0 auto;border:0;width:100%">
-<?php foreach ($finputs as $k => $v): ?>
-<?php if (!$v): continue; endif; ?>
+<?php foreach ($inputs as $k => $v): ?>
 <tr>
 <td style="font-size:14px;color:#A1A2A5;padding:5px 10px"><?php echo $k; ?></td>
 <td style="font-size:14px;color:#555;padding:5px 10px"><?php echo xhentities($v); ?></td>
+</tr>
+<?php endforeach; ?>
+</table>
+</td>
+</tr>
+<?php if (isset($attachments)): ?>
+<tr>
+<td style="background-color:#fff;padding:10px 20px 0">
+<p style="font-size:14px;color:#555;line-height: 22px;margin:0;font-weight:bold">
+Attachments:
+</p>
+</td>
+</tr>
+<tr>
+<td style="background-color:#fff;padding:0 20px">
+<table cellspacing="0" cellpadding="0" style="margin:0 auto;border:0;width:100%">
+<?php foreach ($attachments as $k => $v): ?>
+<tr>
+<td style="font-size:14px;color:#A1A2A5;padding:5px 10px"><?php echo $k; ?></td>
+<td style="padding:5px 10px">
+<a href="<?php echo config_item('base_url'); ?><?php echo $v; ?>" style="font-size:14px;color:#666">Download</a>
+</td>
 </tr>
 <?php endforeach; ?>
 </table>
