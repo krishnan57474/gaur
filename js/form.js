bd49a01751869ed34228daa27be5e3b8a93db0d5
krishnan <krishnan57474@gmail.com>
2018-10-31 17:07:07 +0530

    Updated progress, showerrors, isvalidfile, submit

--- js/form.js
+++ js/form.js
@@ -1,197 +1,307 @@
 (function ($) {
     "use strict";
 
-    var configs, jar, xlock,
+    var jar,
 
     // self overriding fnc
-    progress, showErrors;
+    progress, showErrors, isValidFile, submit;
 
-    function poverride(key, fnc) {
+    function poverride(key, fnc, args) {
         Form.prototype[key] = fnc;
+        fnc.apply(undefined, args);
     }
 
-    function forEach(obj, callback) {
-        Object.keys(obj).forEach(function (k) {
-            callback(obj[k], k, obj);
-        });
+    function getTransitionDuration(elm) {
+        var duration = elm.css("transition-duration").split(",");
+
+        return parseFloat(duration[0]) * 1000;
     }
 
     progress = function () {
-        var prg = $("<div id='prg'></div>"),
-        timer,
-        width;
+        var progressElm,
+        progressBarElm,
+        transitionDuration,
+        timer;
 
-        $(document.body).append(prg);
+        function getProgressFrg() {
+            var progressFrg = $(document.createElement("div"));
+            progressFrg.attr("class", "progress fixed-top");
+            progressFrg.css({ height: "3px" });
 
-        progress = function (display) {
-            prg.css({
-                width: (display ? 0 : 100) + "%",
-                height: (display ? 3 : 0) + "px",
-                borderWidth: (display ? 1 : 0) + "px"
-            });
+            return progressFrg;
+        }
 
-            if (display) {
-                width = 5;
-                clearInterval(timer);
+        function getProgressBarFrg() {
+            var progressBarFrg = $(document.createElement("div"));
+            progressBarFrg.attr("class", "progress-bar bg-warning");
 
-                timer = setInterval(function () {
-                    width += 5;
+            return progressBarFrg;
+        }
 
-                    if (width < 100) {
-                        prg.css({ width: width + "%" });
-                    } else {
-                        clearInterval(timer);
-                    }
-                }, 500);
-            } else {
-                clearInterval(timer);
+        function showProgress() {
+            var width = 5;
+
+            clearInterval(timer);
+            progressElm.css({ width: "100%" });
+            progressBarElm.css({ width: 0 });
+
+            timer = setInterval(function () {
+                width += 5;
+
+                if (width < 100) {
+                    progressBarElm.css({ width: width + "%" });
+                } else {
+                    clearInterval(timer);
+                }
+            }, 500);
+        }
+
+        function hideProgress() {
+            clearInterval(timer);
+            progressBarElm.css({ width: "100%" });
+
+            setTimeout(function () {
+                progressBarElm.css({ width: 0 });
 
                 setTimeout(function () {
-                    prg.css({ width: 0 });
-                }, 1000);
+                    progressElm.css({ width: 0 });
+                }, transitionDuration);
+            }, 1000);
+        }
+
+        function init() {
+            progressElm = getProgressFrg();
+            progressBarElm = getProgressBarFrg();
+            transitionDuration = getTransitionDuration(progressBarElm);
+
+            progressElm.append(progressBarElm);
+            $(document.body).append(progressElm);
+        }
+
+        progress = function (show) {
+            if (show) {
+                showProgress();
+            } else {
+                hideProgress();
             }
         };
 
-        poverride("progress", progress);
-        progress.apply(undefined, arguments);
+        init();
+        poverride("progress", progress, arguments);
     };
 
     showErrors = function () {
-        var elm = $(".j-error", jar),
-        frg = $(document.createDocumentFragment()),
-        errorFrg = $($.parseHTML("<li class='alert alert-danger'><span class='glyphicon glyphicon-remove-sign'></span> </li>")),
-        defaultError = "Invalid request. Please refresh the page or try again later.",
+        var listElm,
+        btnElm,
+        defaultError,
+        errorElm,
+        errorElmPosition,
+        transitionDuration,
+        isAutoHideErrors,
         timer;
 
+        function getListFrg() {
+            var listFrg = $(document.createElement("li"));
+            listFrg.attr("class", "alert alert-danger alert-dismissible fade show");
+
+            return listFrg;
+        }
+
+        function getBtnFrg() {
+            var btnFrg = $(document.createElement("button"));
+            btnFrg.attr("type", "button");
+            btnFrg.attr("class", "close");
+            btnFrg.text("×");
+
+            return btnFrg;
+        }
+
+        function getErrorsFrg(errors) {
+            var errorsFrg = $(document.createDocumentFragment()),
+            frgClone;
+
+            ($.isArray(errors) ? errors : defaultError).forEach(function (errorMsg) {
+                frgClone = listElm.clone();
+                frgClone.text(errorMsg);
+
+                if (isAutoHideErrors) {
+                    frgClone.append(btnElm.clone());
+                } else {
+                    frgClone.removeClass("alert-dismissible");
+                }
+
+                errorsFrg.append(frgClone);
+            });
+
+            return errorsFrg;
+        }
+
+        function hideErrors() {
+            errorElm.children().removeClass("show");
+
+            setTimeout(function () {
+                errorElm.addClass("d-none");
+            }, transitionDuration);
+        }
+
+        function autoHideErrors() {
+            clearTimeout(timer);
+
+            timer = setTimeout(function () {
+                if (!errorElm.hasClass("d-none")) {
+                    hideErrors();
+                }
+            }, 10000);
+        }
+
+        function errorsHandler() {
+            errorElm.on("click", function (e) {
+                if (e.target.tagName === "BUTTON") {
+                    hideErrors();
+                }
+            });
+        }
+
+        function init() {
+            listElm = getListFrg();
+            btnElm = getBtnFrg();
+            defaultError = ["Invalid request. Please refresh the page or try again later!"];
+            errorElm = $(".j-error", jar);
+            errorElmPosition = errorElm.offset() ? errorElm.offset().top : 0;
+            transitionDuration = getTransitionDuration(listElm);
+            isAutoHideErrors = (errorElm.attr("data-show-errors") === undefined);
+
+            errorsHandler();
+        }
+
         showErrors = function (errors) {
-            if (configs.hideErrors) {
-                clearTimeout(timer);
-
-                timer = setTimeout(function () {
-                    elm.addClass("hide");
-                }, 10000);
+            if (isAutoHideErrors) {
+                autoHideErrors();
             }
 
-            if ($.isArray(errors)) {
-                errors.forEach(function (v) {
-                    frg.append(errorFrg.clone().append(v));
-                });
-            } else {
-                frg.append(errorFrg.clone().append(defaultError));
-            }
-
-            elm.children().remove();
-            elm.append(frg).removeClass("hide");
-            $("html, body").animate({ scrollTop: elm.offset().top });
+            errorElm.addClass("d-none").children().remove();
+            errorElm.append(getErrorsFrg(errors)).removeClass("d-none");
+            $("html, body").animate({ scrollTop: errorElmPosition });
         };
 
-        poverride("showErrors", showErrors);
-        showErrors.apply(undefined, arguments);
+        init();
+        poverride("showErrors", showErrors, arguments);
     };
 
-    function isValidFile(file, atypes, limit) {
-        var rx = new RegExp("^" + atypes.join("$|^") + "$", "i");
+    isValidFile = function () {
+        var invalidFileError = ["The filetype you are attempting to upload is not allowed."],
+        filesizeError = ["The uploaded file exceeds the maximum upload file size limit."];
 
-        if (!rx.test(file.name.split(".").pop())) {
-            showErrors(["The filetype you are attempting to upload is not allowed."]);
-            return false;
+        function toBytes(val) {
+            var units = "bkmgtpezy",
+            size = val.match(/[\d.]+/g),
+            unit = val.replace(size, "");
+
+            size = parseFloat(size);
+            unit = unit[0].toLowerCase();
+
+            return size * Math.pow(1024, units.indexOf(unit));
         }
 
-        if (!file.size || file.size > limit) {
-            showErrors(["The uploaded file exceeds the maximum upload file size limit."]);
-            return false;
-        }
+        isValidFile = function (file, atypes, limit, error) {
+            var rx = new RegExp("^" + atypes.join("$|^") + "$", "i");
 
-        return true;
-    }
-
-    function submit(xconfigs) {
-        if (xlock) {
-            return;
-        }
-
-        var data = {
-            method: "POST",
-            url: xconfigs.url || location.href,
-            error: function () {
-                if (xconfigs.load) {
-                    xconfigs.load();
-                }
-
-                if (!xconfigs.ignoreErrors) {
-                    showErrors();
-                }
-
-                progress(false);
-                xlock = false;
-            },
-            data: xconfigs.data
-        };
-
-        if (xconfigs.upload) {
-            data.processData = false;
-            data.contentType = false;
-        }
-
-        xlock = true;
-        $(".j-error", jar).addClass("hide");
-        progress(true);
-
-        $.ajax(data).done(function (response, status) {
-            if (xconfigs.load) {
-                xconfigs.load();
+            if (!rx.test(file.name.split(".").pop()) || !file.size) {
+                (error || showErrors)(invalidFileError);
+                return false;
             }
 
-            progress(false);
+            if (file.size > toBytes(limit)) {
+                (error || showErrors)(filesizeError);
+                return false;
+            }
+
+            return true;
+        };
+
+        poverride("isValidFile", isValidFile, arguments);
+    };
+
+    submit = function () {
+        var errorElm = $(".j-error", jar),
+        lock;
+
+        function getConfigs(uconfigs) {
+            var configs = {
+                method: "POST",
+                url: uconfigs.url || location.href,
+                error: function () {
+                    if (uconfigs.load) {
+                        uconfigs.load();
+                    }
+
+                    if (!uconfigs.ignoreErrors) {
+                        (uconfigs.error || showErrors)();
+                    }
+
+                    (uconfigs.progress || progress)(false);
+                    lock = false;
+                },
+                data: uconfigs.data
+            };
+
+            if (uconfigs.upload) {
+                configs.processData = false;
+                configs.contentType = false;
+            }
+
+            return configs;
+        }
+
+        function responseHandler(uconfigs, response, status) {
+            if (uconfigs.load) {
+                uconfigs.load();
+            }
+
+            (uconfigs.progress || progress)(false);
 
             if (status !== "success" || !response.status || response.errors) {
-                if (!xconfigs.ignoreErrors) {
-                    showErrors(response.errors);
+                if (!uconfigs.ignoreErrors) {
+                    (uconfigs.error || showErrors)(response.errors);
                 }
 
-                xlock = false;
+                lock = false;
                 return;
             }
 
-            xconfigs.success(response.data);
-            xlock = false;
-        });
-    }
+            uconfigs.success(response.data);
+            lock = false;
+        }
 
-    function init(uconfigs) {
-        $ = jQuery;
-        configs = {
-            hideErrors: true
+        submit = function (uconfigs, ignoreLock) {
+            if (!ignoreLock && lock) {
+                return;
+            }
+
+            lock = true;
+            errorElm.addClass("d-none");
+            (uconfigs.progress || progress)(true);
+
+            $.ajax(getConfigs(uconfigs)).done(function (response, status) {
+                responseHandler(uconfigs, response, status);
+            });
         };
 
-        if (uconfigs) {
-            forEach(uconfigs, function (v, k) {
-                if (configs.hasOwnProperty(k)) {
-                    configs[k] = v;
-                }
-            });
-        }
+        poverride("submit", submit, arguments);
+    };
 
+    function init() {
+        $ = jQuery;
         jar = $("#j-ar");
-
-        if (configs.hideErrors) {
-            $(".j-error", jar).on("click", function () {
-                $(this).addClass("hide");
-            });
-        }
     }
 
     function Form() {}
 
     Form.prototype = {
-        init: init,
-        submit: submit,
-        isValidFile: isValidFile,
-        showErrors: showErrors,
         progress: progress,
-
-        get lock() { return xlock; },
-        set lock(status) { xlock = status; }
+        showErrors: showErrors,
+        isValidFile: isValidFile,
+        submit: submit
     };
 
     function GForm() {
@@ -202,5 +312,6 @@
         return new GForm();
     }
 
+    init();
     window.GForm = GForm;
 }());
