991f0507137b3cfdd8afbbbb9b75964b1636ee53
krishnan <krishnan57474@gmail.com>
2019-09-13 12:57:58 +0530

    Seperated CI controller

--- src/gaur/Controller.php
+++ src/gaur/Controller.php
@@ -4,33 +4,10 @@ declare(strict_types=1);
 
 namespace Gaur;
 
-use CodeIgniter\{
-    Controller as CIController,
-    HTTP\RequestInterface,
-    HTTP\ResponseInterface
-};
-use Config\Services;
-use Psr\Log\LoggerInterface;
+use Gaur\Controller\ControllerAbstract;
 
-abstract class Controller extends CIController
+abstract class Controller extends ControllerAbstract
 {
-    /**
-     * Helpers to be loaded automatically
-     *
-     * @var array
-     */
-    protected $helpers = [
-        'csp',
-        'html'
-    ];
-
-    /**
-     * Session object
-     *
-     * @var \CodeIgniter\Session\Session
-     */
-    protected $session;
-
     /**
      * Default page for this controller
      *
@@ -48,25 +25,5 @@ abstract class Controller extends CIController
     public function _remap(string ...$args): void
     {
         $this->index(...$args);
-    }
-
-    /**
-     * Initialize controller
-     *
-     * @param RequestInterface  $request  request object
-     * @param ResponseInterface $response response object
-     * @param LoggerInterface   $logger   logger object
-     *
-     * @return void
-     */
-    public function initController(
-        RequestInterface $request,
-        ResponseInterface $response,
-        LoggerInterface $logger
-    )
-    {
-        parent::initController($request, $response, $logger);
-
-        $this->session = Services::session();
     }
 }
