588ac512249d03f83d132b79282547ceee093767
krishnan <krishnan57474@gmail.com>
2019-09-26 23:30:32 +0530

    Updated controller abstract

--- src/gaur/Controller.php
+++ src/gaur/Controller.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Gaur;
 
-use Gaur\Controller\ControllerAbstract;
+use CodeIgniter\Controller as BaseController;
+use Config\Services;
 
-abstract class Controller extends ControllerAbstract
+abstract class Controller extends BaseController
 {
+    /**
+     * Helpers to be loaded automatically
+     *
+     * @var array
+     */
+    protected $helpers = [
+        'csp',
+        'html'
+    ];
+
     /**
      * Default page for this controller
      *
@@ -24,6 +35,8 @@ abstract class Controller extends ControllerAbstract
      */
     public function _remap(string ...$args): void
     {
+        Services::session();
+
         $this->index(...$args);
     }
 }
