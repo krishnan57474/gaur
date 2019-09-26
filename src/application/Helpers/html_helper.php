e4973afd2f7908a9449e3f28f3cc0932647658e6
krishnan <krishnan57474@gmail.com>
2019-09-26 23:30:09 +0530

    Removed xhtml helper

--- src/application/Helpers/html_helper.php
+++ src/application/Helpers/html_helper.php
@@ -13,15 +13,3 @@ function hentities(string $string): string
 {
     return htmlentities($string, ENT_QUOTES | ENT_HTML5, 'UTF-8', true);
 }
-
-/**
- * Convert characters to XHTML entities
- *
- * @param string $string string to convert
- *
- * @return string
- */
-function xhentities(string $string): string
-{
-    return htmlentities($string, ENT_QUOTES | ENT_XHTML, 'UTF-8', true);
-}
