faa74135b5d1e3471e4bf0839cb06c68ea92c1d4
krishnan <krishnan57474@gmail.com>
2019-10-22 14:09:41 +0530

    Fix incorrect view path

--- src/application/Views/app/admin/users/edit.php
+++ src/application/Views/app/admin/users/edit.php
@@ -1,17 +1,17 @@
-    <?= view('app/default/common/head_top') ?>
+    <?= view('app/admin/common/head_top') ?>
 
     <title>Edit a user - <?= config('Config\App')->siteName ?></title>
 
     <!-- meta for search engines -->
     <meta name="robots" content="noindex">
 
-    <?= view('app/default/common/css') ?>
-    <?= view('app/default/common/head_bottom') ?>
-    <?= view('app/default/common/menu') ?>
+    <?= view('app/admin/common/css') ?>
+    <?= view('app/admin/common/head_bottom') ?>
+    <?= view('app/admin/common/menu') ?>
 
     <main class="container" id="j-ar">
         <div class="row">
-            <div class="col-sm-12">
+            <div class="col-md-12">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item">
                         <a href="">Home</a>
@@ -88,7 +88,7 @@
         </div>
     </main>
 
-    <?= view('app/default/common/foot_top') ?>
+    <?= view('app/admin/common/foot_top') ?>
 
     <script nonce="<?= getCspNonce() ?>">
     (function ($) {
@@ -137,5 +137,5 @@
 
     <script type="text/x-async-js" data-src="js/form.js" class="j-ajs"></script>
 
-    <?= view('app/default/common/js') ?>
-    <?= view('app/default/common/foot_bottom') ?>
+    <?= view('app/admin/common/js') ?>
+    <?= view('app/admin/common/foot_bottom') ?>
