0d2ce10d06b51680e5e9c24739624827c317a519
krishnan <krishnan57474@gmail.com>
2019-10-24 19:29:26 +0530

    Added image library

---
+++ src/gaur/Image/Image.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gaur\Image;
+
+use Config\Services;
+
+class Image
+{
+    /**
+     * Create thumbnail
+     *
+     * $config fields
+     *
+     * int     height   height of the new image
+     * string  library  image library (gd, imagick)
+     * string  ratio    aspect ratio dimension (auto, height, width)
+     * int     width    width of the new image
+     *
+     * @param string $filename source filename
+     * @param string $path     destination path
+     * @param array  $uconfig  user configuration
+     *
+     * @return bool
+     */
+    public function createThumb(
+        string $filename,
+        string $path,
+        array $uconfig = []
+    ): bool
+    {
+        $config            = [];
+        $config['height']  = 256;
+        $config['library'] = 'gd';
+        $config['ratio']   = 'auto';
+        $config['width']   = 256;
+
+        foreach ($uconfig as $k => $v) {
+            $config[$k] = $v;
+        }
+
+        if (substr($path, -1) !== '/') {
+            $path .= '/';
+        }
+
+        $image = Services::image($config['library']);
+
+        $image->withFile($filename);
+
+        $image->resize(
+            $config['width'],
+            $config['height'],
+            true,
+            $config['ratio']
+        );
+
+        $status = $image->save(
+            $path
+            . basename($filename)
+        );
+
+        return $status;
+    }
+}
