ffba2a616308d0d1e81b065ec7382f5c8f2665c9
krishnan <krishnan57474@gmail.com>
2019-11-09 16:06:10 +0530

    Added csp options

--- src/application/Helpers/csp_helper.php
+++ src/application/Helpers/csp_helper.php
@@ -2,22 +2,36 @@
 
 declare(strict_types=1);
 
-use App\Data\Security\CspConfig;
 use Gaur\Security\ContentSecurityPolicy;
 
 /**
  * Get content security policy
  *
+ * @param string $cspConfig   csp config class name
+ * @param bool   $allowScript allow script tag to execute
+ * @param bool   $allowStyle  allow style tag to execute
+ *
  * @return string
  */
-function getCsp(): string
+function getCsp(
+    string $cspConfig,
+    bool $allowScript = false,
+    bool $allowStyle = false
+): string
 {
-    $config = new CspConfig();
+    $config = new $cspConfig();
     $csp    = new ContentSecurityPolicy();
     $nonce  = $csp->getNonce();
 
-    $config->scriptSrc   = $config->scriptSrc ?? [];
-    $config->scriptSrc[] = '\'nonce-' . $nonce . '\'';
+    if ($allowScript) {
+        $config->scriptSrc   = $config->scriptSrc ?? [];
+        $config->scriptSrc[] = '\'nonce-' . $nonce . '\'';
+    }
+
+    if ($allowStyle) {
+        $config->styleSrc   = $config->styleSrc ?? [];
+        $config->styleSrc[] = '\'nonce-' . $nonce . '\'';
+    }
 
     return $csp->get($config);
 }
