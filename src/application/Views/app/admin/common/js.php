3acf0c7bcb214b91ac4c67a911a3fc124744b3e7
krishnan <krishnan57474@gmail.com>
2020-02-13 17:45:16 +0530

    updated cdn link, fix nested menu

--- src/application/Views/app/admin/common/js.php
+++ src/application/Views/app/admin/common/js.php
@@ -12,7 +12,11 @@
             match = false;
 
             while (++i < l) {
-                if (base[i] === current[i] && (i + 1) >= l) {
+                if (base[i] !== current[i]) {
+                    break;
+                }
+
+                if ((i + 1) >= l) {
                     match = true;
                     break;
                 }
@@ -25,30 +29,51 @@
             var base = segment(location.href.replace(document.baseURI, "")),
             current;
 
-            $(".g-main-menu > li > a").each(function (k, elm) {
-                current = $(elm).attr("href");
+            $(".g-main-menu").each(function () {
+                $("> li > a", this).each(function (k, elm) {
+                    current = $(elm).attr("href");
 
-                if (current === undefined) {
-                    return;
-                }
+                    if (compareSegment(base, segment(current))) {
+                        $(elm).addClass("active");
+                        return false;
+                    }
+                });
+            });
+        }
 
-                if (compareSegment(base, segment(current))) {
-                    $(elm).addClass("active");
-                    return false;
-                }
+        function initMenu() {
+            var inputFrg = $('<input class="g-menu-state" type="checkbox">'),
+            labelFrg = $('<label class="g-menu-icon"></label>'),
+            i = 1,
+            childElm,
+            id;
+
+            $(".g-sub-menu").each(function () {
+                childElm = $(this).parent().children();
+                id = "menu-state-" + i;
+                i += 1;
+
+                childElm.first().before(
+                    inputFrg.clone().attr("id", id)
+                );
+
+                childElm.last().after(
+                    labelFrg.clone().attr("for", id)
+                );
             });
         }
 
         function init() {
             $ = jQuery;
             setActiveMenu();
+            initMenu();
         }
 
         (window._jq = window._jq || []).push(init);
     })();
     </script>
 
-    <script type="text/x-async-css" data-src="https://cdn.jsdelivr.net/gh/iconic/open-iconic@1.1.1/font/css/open-iconic-bootstrap.min.css" data-integrity="sha384-wWci3BOzr88l+HNsAtr3+e5bk9qh5KfjU6gl/rbzfTYdsAVHBEbxB33veLYmFg/a" class="j-acss"></script>
+    <script type="text/x-async-css" data-src="https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@5.12.1/css/all.min.css" data-integrity="sha384-v8BU367qNbs/aIZIxuivaU55N5GPF89WBerHoGA4QTcbUjYiLQtKdrfXnqAcXyTv" class="j-acss"></script>
 
     <script type="text/x-async-js" data-src="https://cdn.jsdelivr.net/gh/jquery/jquery@3.4.1/dist/jquery.min.js" data-integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" data-order="1" class="j-ajs"></script>
 
