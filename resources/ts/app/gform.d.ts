5de7f4f678faf5413d96c25718b56cf9c9df5834
krishnan <krishnan57474@gmail.com>
2020-05-03 19:12:28 +0530

    updated gform interface

--- resources/ts/app/gform.d.ts
+++ resources/ts/app/gform.d.ts
@@ -1,11 +1,21 @@
-interface AjaxUserConfigsInterface {
-    context: JQuery<HTMLElement>;
-    data: Record<string, string | number | Array<string>>;
-    load?: VoidFunction;
-    method?: string;
-    success: (data: string) => void;
+interface AjaxResponse {
+    data: AjaxResponseDataType | null;
+    errors: Array<string> | null;
+    isJson: boolean;
+    response: AjaxResponseInterface | null;
+    header(name: string): null | string;
 }
 
-interface GForm {
-    submit: (uconfigs: AjaxUserConfigsInterface, ignoreLock?: boolean) => void;
+interface Ajax {
+    data(data: AjaxConfigDataType): this;
+    headers(headers: Record<string, string>): this;
+    on(type: string, listener: Function): this;
+    send(): Promise<AjaxResponse>;
+    upload(status: boolean): this;
+}
+
+declare class GForm {
+    error(errors: Array<string> | string, context: HTMLElement): void;
+    progress(status: boolean): void;
+    request(method: string, url: string, ignoreLock?: boolean): Ajax;
 }
