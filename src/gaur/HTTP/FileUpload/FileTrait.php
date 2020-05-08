d8769d3a0627c26d3ab0f5ea4438d48c58247fb7
krishnan <krishnan57474@gmail.com>
2020-05-08 16:40:04 +0530

    added file handling

--- src/gaur/HTTP/FileUpload/FileTrait.php
+++ src/gaur/HTTP/FileUpload/FileTrait.php
@@ -4,20 +4,59 @@ declare(strict_types=1);
 
 namespace Gaur\HTTP\FileUpload;
 
+use Gaur\HTTP\Input;
+use UPLOAD_ERR_OK;
+
 trait FileTrait
 {
     /**
+     * Get uploaded files
+     *
+     * @param string $afield    attachment field name
+     * @param int    $count     number of files to move (0 for all)
+     * @param bool   $keepIndex preserve array keys
+     *
+     * @return mixed[][]
+     */
+    protected function getFiles(string $afield, int $count, bool $keepIndex): array
+    {
+        $files     = [];
+        $filesList = array_slice(
+            Input::files($afield),
+            0,
+            $count ?: null,
+            $keepIndex
+        );
+
+        foreach ($filesList as $k => $item) {
+            if (!is_uploaded_file($item['tmp_name'])
+                || $item['error'] !== UPLOAD_ERR_OK
+                || !$item['size']
+            ) {
+                continue;
+            }
+
+            if ($keepIndex) {
+                $files[$k] = $item;
+            } else {
+                $files[] = $item;
+            }
+        }
+
+        return $files;
+    }
+
+    /**
      * Move uploaded file
      *
-     * @param array  $file file to move
-     * @param string $path destination path
+     * @param mixed[] $file file to move
+     * @param string  $path destination path
      *
      * @return string
      */
     protected function moveFile(array $file, string $path): string
     {
         $fileExt  = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
-        $fileExt  = '.' . $fileExt;
         $filename = $this->getNewFilename($fileExt, $path);
 
         if (!@copy($file['tmp_name'], $path . $filename)
@@ -40,8 +79,8 @@ trait FileTrait
     {
         $units   = 'bkmgtpezy';
         $size    = (float)$unit;
-        $uprefix = substr($unit, -2, 1);
-        $uindex  = strpos($units, strtolower($uprefix ?: 'b'));
+        $uprefix = preg_replace('/[^a-zA-Z]+/', '', $unit);
+        $uindex  = strpos($units, strtolower($uprefix[0] ?? 'b'));
 
         if (is_bool($uindex)) {
             $uindex = 0;
