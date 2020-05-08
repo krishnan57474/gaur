7a74f85e69e56e9a0514ef81b7903a38b447bd52
krishnan <krishnan57474@gmail.com>
2020-05-08 16:37:29 +0530

    added data getter / setter

---
+++ src/application/Helpers/data_helper.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Get json content from data directory
+ *
+ * @param string $path file path of json content
+ *
+ * @return array
+ */
+function getDataContents(string $path): array
+{
+    $path = APPPATH . 'Data/' . $path . '.json';
+
+    $data = json_decode(
+        file_get_contents($path),
+        true
+    );
+
+    return $data;
+}
+
+/**
+ * Update json content of data directory
+ *
+ * @param string $path file path of json content
+ * @param array  $data file data
+ *
+ * @return void
+ */
+function putDataContents(string $path, array $data): void
+{
+    $path = APPPATH . 'Data/' . $path . '.json';
+
+    file_put_contents(
+        $path,
+        json_encode($data)
+    );
+}
