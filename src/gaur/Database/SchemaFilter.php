9c8a9f0cfbda46a9df27c869c215f0c07fbbd895
krishnan <krishnan57474@gmail.com>
2020-07-26 15:49:55 +0530

    updated filter

--- src/gaur/Database/SchemaFilter.php
+++ src/gaur/Database/SchemaFilter.php
@@ -34,75 +34,47 @@ class SchemaFilter
                 continue;
             }
 
-            switch ($rule['type']) {
-                case SchemaTypeGroup::INTEGER: {
-                    if (is_int($v)) {
-                        $data[$k] = $v;
-                    } else {
-                        $data[$k] = (int)$v;
-                    }
-
-                    break;
+            if ($rule['type'] === SchemaTypeGroup::INTEGER) {
+                if (is_int($v)) {
+                    $data[$k] = $v;
+                } else {
+                    $data[$k] = (int)$v;
                 }
-
-                case SchemaTypeGroup::FLOAT: {
-                    if (is_float($v)) {
-                        $data[$k] = $v;
-                    } else {
-                        $data[$k] = (float)$v;
-                    }
-
-                    break;
+            } elseif ($rule['type'] === SchemaTypeGroup::FLOAT) {
+                if (is_float($v)) {
+                    $data[$k] = $v;
+                } else {
+                    $data[$k] = (float)$v;
                 }
-
-                case SchemaTypeGroup::DATE: {
-                    if (preg_match('/^\d{4}\-\d{2}\-\d{2}$/', $v)) {
-                        $data[$k] = $v;
-                    } else {
-                        $data[$k] = '0000-00-00';
-                    }
-
-                    break;
+            } elseif ($rule['type'] === SchemaTypeGroup::DATE) {
+                if (preg_match('/^\d{4}\-\d{2}\-\d{2}$/', $v)) {
+                    $data[$k] = $v;
+                } else {
+                    $data[$k] = '0000-00-00';
                 }
-
-                case SchemaTypeGroup::DATETIME: {
-                    if (preg_match('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $v)) {
-                        $data[$k] = $v;
-                    } else {
-                        $data[$k] = '0000-00-00 00:00:00';
-                    }
-
-                    break;
+            } elseif ($rule['type'] === SchemaTypeGroup::DATETIME) {
+                if (preg_match('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $v)) {
+                    $data[$k] = $v;
+                } else {
+                    $data[$k] = '0000-00-00 00:00:00';
                 }
-
-                case SchemaTypeGroup::TIME: {
-                    if (preg_match('/^\d{2}:\d{2}:\d{2}$/', $v)) {
-                        $data[$k] = $v;
-                    } else {
-                        $data[$k] = '00:00:00';
-                    }
-
-                    break;
+            } elseif ($rule['type'] === SchemaTypeGroup::TIME) {
+                if (preg_match('/^\d{2}:\d{2}:\d{2}$/', $v)) {
+                    $data[$k] = $v;
+                } else {
+                    $data[$k] = '00:00:00';
                 }
-
-                case SchemaTypeGroup::STRING: {
-                    if (is_string($v)) {
-                        $data[$k] = $v;
-                    } else {
-                        $data[$k] = (string)$v;
-                    }
-
-                    break;
+            } elseif ($rule['type'] === SchemaTypeGroup::STRING) {
+                if (is_string($v)) {
+                    $data[$k] = $v;
+                } else {
+                    $data[$k] = (string)$v;
                 }
-
-                case SchemaTypeGroup::JSON: {
-                    if (is_array($v) || is_object($v)) {
-                        $data[$k] = json_encode($v);
-                    } elseif (is_string($v)) {
-                        $data[$k] = json_decode($v, true);
-                    }
-
-                    break;
+            } elseif ($rule['type'] === SchemaTypeGroup::JSON) {
+                if (is_array($v) || is_object($v)) {
+                    $data[$k] = json_encode($v);
+                } elseif (is_string($v)) {
+                    $data[$k] = json_decode($v, true);
                 }
             }
         }
