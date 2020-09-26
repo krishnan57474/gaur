94a640eaf33a2819dd09f61e874f170d328eae2b
krishnan <krishnan57474@gmail.com>
2020-09-26 14:38:21 +0530

    added reset update

--- src/application/Models/Users/UserReset.php
+++ src/application/Models/Users/UserReset.php
@@ -35,19 +35,39 @@ class UserReset extends Model
      * Get user reset
      *
      * @param string $token token
+     * @param int    $type  token type
      *
      * @return mixed[]|null
      */
-    public function get(string $token): ?array
+    public function get(string $token, int $type): ?array
     {
         $qry = 'SELECT *
                 FROM `' . $this->db->prefixTable('user_resets') . '`
-                WHERE `token` = ' . $this->db->escape($token);
+                WHERE `token` = ' . $this->db->escape($token)
+                    . ' AND `type` = ' . $type;
 
         $rdata = $this->db->query($qry)->getRowArray();
         $data  = $rdata ? (new UserResetSchema())->filter($rdata) : $rdata;
 
         return $data;
+    }
+
+    /**
+     * Get user reset id
+     *
+     * @param int $uid  user id
+     * @param int $type token type
+     *
+     * @return int
+     */
+    public function getID(int $uid, int $type): int
+    {
+        $qry = 'SELECT `id`
+                FROM `' . $this->db->prefixTable('user_resets') . '`
+                WHERE `uid` = ' . $uid
+                    . ' AND `type` = ' . $type;
+
+        return (int)($this->db->query($qry)->getRowArray()['id'] ?? 0);
     }
 
     /**
@@ -61,6 +81,23 @@ class UserReset extends Model
     {
         $qry = 'DELETE FROM `' . $this->db->prefixTable('user_resets') . '`
                 WHERE `id` = ' . $id;
+
+        $this->db->query($qry);
+    }
+
+    /**
+     * Update user reset
+     *
+     * @param int    $id    reset id
+     * @param string $token token
+     *
+     * @return void
+     */
+    public function update(int $id, string $token): void
+    {
+        $qry = 'UPDATE `' . $this->db->prefixTable('user_resets') . '`
+                SET `token` = ' . $this->db->escape($token)
+                . ' WHERE `id` = ' . $id;
 
         $this->db->query($qry);
     }
