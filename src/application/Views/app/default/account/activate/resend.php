3f29092775ba32a7a76f2e5d6cc4a6258d029bcf
krishnan <krishnan57474@gmail.com>
2020-09-26 14:40:53 +0530

    added reset resend

---
+++ src/application/Views/app/default/account/activate/resend.php
@@ -0,0 +1,46 @@
+    <?= view('app/default/common/head_top') ?>
+
+    <meta http-equiv="Content-Security-Policy" content="<?= getCsp('Config', true) ?>">
+
+    <title>Resend activation - <?= config('Config\App')->siteName ?></title>
+
+    <!-- meta for search engines -->
+    <meta name="robots" content="noindex">
+
+    <?= view('app/default/common/css') ?>
+    <?= view('app/default/common/head_bottom') ?>
+    <?= view('app/default/common/menu') ?>
+
+    <main class="container" id="j-ar">
+        <div class="row justify-content-center">
+            <div class="col-sm-9 col-md-7 col-lg-5">
+                <h1 class="text-center">Resend activation</h1>
+
+                <ul class="list-unstyled j-error d-none"></ul>
+                <p class="alert alert-success j-success d-none"></p>
+
+                <form method="post" data-url="account/activate/resend">
+                    <div class="form-group">
+                        <label>Username / Email address <span class="text-danger">*</span></label>
+                        <input class="form-control" name="identity" type="text" required>
+                    </div>
+
+                    <div class="form-row align-items-center form-group">
+                        <div class="col-5">
+                            <input name="<?= $csrf['name'] ?>" type="hidden" value="<?= $csrf['hash'] ?>">
+                            <input class="btn btn-block btn-primary" type="submit" value="Resend">
+                        </div>
+                        <div class="col-7 text-right">
+                            <span class="fas fa-sign-in-alt"></span>
+                            <a href="account/login">Sign in</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <?= view('app/default/common/foot_top') ?>
+    <?= view('app/default/common/js/form') ?>
+    <?= view('app/default/common/js') ?>
+    <?= view('app/default/common/foot_bottom') ?>
