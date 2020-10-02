dfdc2e92be9a69f2458982a85aa66855a882019e
krishnan <krishnan57474@gmail.com>
2020-10-02 12:42:26 +0530

    removed unnecessary

--- src/application/Views/app/admin/common/app/content_top.php
+++ src/application/Views/app/admin/common/app/content_top.php
@@ -1,6 +1,6 @@
     <div data-jitem="confirm" class="alert alert-warning text-center text-sm-left d-none">
         <div class="row align-items-center">
-            <div class="col-sm-6 mb-2 mb-sm-0" data-jitem="confirm-msg">Confirm change status</div>
+            <div class="col-sm-6 mb-2 mb-sm-0" data-jitem="confirm-msg"></div>
             <div class="col-sm-6 text-sm-right">
                 <button data-action="confirm" type="button" class="btn btn-success">Confirm</button>
                 <button data-action="cancel" type="button" class="btn btn-secondary">Cancel</button>
